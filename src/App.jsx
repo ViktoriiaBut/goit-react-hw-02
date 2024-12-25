f878ad5bb845a83cf58ba0711d14f8cc91248032
ViktoriiaBut <zironka.tory@gmail.com>
2024-12-25 01:15:26 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useEffect } from 'react'
+import Description from './components/Description/Description';
+import Feedback from './components/Feedback/Feedback';
+import Options from './components/Options/Options';
+import Notification from "./components/Notifications/Notifications";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+  const [feedback, setFeedback] = useState (
+    () =>
+   JSON.parse(localStorage.getItem("feedback")) || {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  }
+  );
+
+const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+const positiveFeedback =  totalFeedback === 0 ? 0 : Math.round((feedback.good / totalFeedback) * 100);
+
+useEffect (() => {
+  localStorage.setItem("feedback", JSON.stringify(feedback));
+}, [feedback]
+);
+
+const updateFeedback = (feedbackType) => {
+  setFeedback((prev) => ({
+    ...prev,
+    [feedbackType]: prev[feedbackType] + 1
+  })
+);
+};
+
+const resetFeedback = () => {
+  setFeedback({ good: 0, neutral: 0, bad: 0 });
+};
+
+return (
+ <div>
+  <Description />
+  <Options 
+  onLeaveFeedback={updateFeedback}
+  onReset={resetFeedback}
+  totalFeedback={totalFeedback}
+  />
+  {totalFeedback > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      )
+      : (
+        <Notification message="No feedback given" />
+      )}
+    </div>
+  );
+};
+
 
 export default App
